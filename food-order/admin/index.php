53e695a6e2b3fd535c1fa71169e2b9ac34e906f7
Kavindu Anjana <80504941+kavinduanjana123@users.noreply.github.com>
2022-08-11 00:50:15 +0530

    Add New Project

---
+++ food-order/admin/index.php
@@ -0,0 +1,43 @@
+<html>
+    <head>
+    <title>Food Odrer website -Home page</title>
+
+
+    <link rel="stylesheet" href="../css/admin.css">
+    </head>
+
+        <body>
+            <!-- Menu Section Starts -->
+            <div class="menu text-center">
+                <div class="wrapper">
+                    <ul>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Admin</a></li>
+                        <li><a href="">Category</a></li>
+                        <li><a href="">Food</a></li>
+                        <li><a href="">Order</a></li>
+
+                    </ul>
+                </div>
+                
+            </div>
+
+            <!-- Menu Section Ends -->
+
+            <!-- Main content Section Starts -->
+            <div class="main_content">
+                <div class="wrapper">
+                    Main Content Goes Here
+                </div>
+            </div>
+            <!-- Main contant Section End -->
+
+            <!-- footer content Section Starts -->
+            <div class="footer">
+                <div class="wrapper">
+                    <P class="text-center" >2022 ALL RIGHTS RESERVED,Developed by <a href="https://www.facebook.com/kavindu.anjana.3"> Kavindu Anjana</a></P>
+                </div>
+            </div>
+            <!-- footer contant Section End -->
+        </body>
+</html>
